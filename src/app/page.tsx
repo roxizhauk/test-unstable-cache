b3edef116eacbc6e4a21283855abb893f355d3ff
Roxi Zhauk <roxizhauk@gmail.com>
2024-01-15 17:27:41 +0900

    small change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
 
   return (
     <>
-      <div>List</div>
+      <div>LIST</div>
       <div className="grid grid-cols-2 gap-2">
         {options.map(({ word, num }) => (
           <Fragment key={word + num}>
