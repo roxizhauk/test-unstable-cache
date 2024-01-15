ad350bd8d30ce8f0cec2b07e43d4c9b76f3e1ed2
Roxi Zhauk <roxizhauk@gmail.com>
2024-01-15 15:58:58 +0900

    fixed as expected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import { Fragment } from "react";
 export const dynamic = "force-dynamic";
 export default async function Home() {
   // const options = await getWords();
-  const options: { word: string; num: number }[] = await fetch("/api/words").then((res) =>
-    res.json(),
-  );
+  const options: { word: string; num: number }[] = await fetch(
+    "https://my-next-lab.vercel.app//api/words",
+  ).then((res) => res.json());
 
   if (!options) return <>Loading...</>;
   return (
